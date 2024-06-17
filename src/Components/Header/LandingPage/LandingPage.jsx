4c552138e75c620445d13b1602347f500f3c534e
alfonsín <ponchodeandan@gmail.com>
2024-06-16 22:48:13 -0600

    update

---
+++ src/Components/Header/LandingPage/LandingPage.jsx
@@ -0,0 +1,10 @@
+import "./landingPage.scss";
+const LandingPage = () => {
+    return (
+        <div className="photoWrapper">
+            <img src="/chaBots2019photo.png" alt=""/>
+        </div>
+    )
+}
+
+export default LandingPage;
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,12 @@
 import Header from '../../Components/Header/Header';
+import LandingPage from '../../Components/Header/LandingPage/LandingPage';
 //import Sidebar from '../../Components/Sidebar/Sidebar';
 import "./landing.scss";
 const Landing = () => {
     return (
         <div>
             <Header/>
+            <LandingPage/>
             <h2>Landing</h2>
         </div>
     )
